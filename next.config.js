29e296bfe22c12471e85b155d71a7470ce58bd76
Gabor Siffer <gabor.siffer@gmail.com>
2023-06-21 16:51:43 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  // Other Next.js configuration options...
+
+  webpack: (config, { isServer }) => {
+    // Add the CopyWebpackPlugin to the plugins array
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, "data"),
+            to: path.join(__dirname, ".next/data"),
+          },
+        ],
+      })
+    );
+
+    // Return the updated configuration
+    return config;
+  },
+};
+
+module.exports = nextConfig;
