e21c514606efa7ced0e4e79a12fc650c08790754
Gabor Siffer <gabor.siffer@gmail.com>
2023-06-21 17:36:08 -0700

    update

--- next.config.js
+++ next.config.js
@@ -5,20 +5,6 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
-
-  async headers() {
-    return [
-      {
-        source: "/data/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=3600",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
